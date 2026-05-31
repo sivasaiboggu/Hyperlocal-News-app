5ab6fbccee8df67789ac0bb177b1874fc40b9778
sivasaiboggu <shivasai1872005115@gmail.com>
2026-05-31 21:53:38 +0530

    chore: add immer to transformIgnorePatterns in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|@reduxjs|redux-persist)/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@reduxjs|redux-persist|immer)/)',
   ],
 };
